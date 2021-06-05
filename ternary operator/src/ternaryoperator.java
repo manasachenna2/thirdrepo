bd47fe42acd4986f7066aa2f57ba0e65d21baac2
manas <manas@DESKTOP-OU43GB8>
2021-06-05 15:44:04 +0530

    example of ternary operator

---
+++ ternary operator/src/ternaryoperator.java
@@ -0,0 +1,15 @@
+
+public class ternaryoperator {
+
+	public static void main(String[] args) {
+		int  februarydays = 29;
+		String result;
+		//ternary operator 
+		result =(februarydays==28)?"Not a Leap year" :"Leap year";
+		System .out.println(result );
+		
+		
+
+	}
+
+}
