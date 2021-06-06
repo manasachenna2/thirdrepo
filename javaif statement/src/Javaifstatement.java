db9750fbfba9e3aecd2e7659d0f9dad60b1c3dc7
manas <manas@DESKTOP-OU43GB8>
2021-06-06 12:21:00 +0530

    java if statement example

---
+++ javaif statement/src/Javaifstatement.java
@@ -0,0 +1,15 @@
+
+public class Javaifstatement {
+
+	public static void main(String[] args) {
+		int  number = 10;
+		//check  if number is greater than 0
+		if (number>0) {
+			System.out.println("The number is positive.");
+		}
+		System.out.println("Statement outside if block");
+		}
+
+	}
+
+
